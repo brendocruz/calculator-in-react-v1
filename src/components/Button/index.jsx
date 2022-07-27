76837a87bdf3c1fe3919666c7d49d678da3a4283
Brendo Cruz <43189865+brendocruz@users.noreply.github.com>
2022-07-27 15:42:01 -0300

    Initial layout and style for Calculator and Button components.

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,5 +1,9 @@
+import './style.css'
 
-
-export function Button() {
-	return <h2>A button</h2>;
+export function Button({ text, type, styleClasses }) {
+	return (
+		<div onClick={() => { console.log(1) }} className={"button-container " + styleClasses}>
+			{text}
+		</div>
+	);
 }
--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -1,11 +1,38 @@
-import { Button } from '../Button';
+import "./style.css";
+import { Button } from "../Button";
 
 export function Calculator() {
 	return (
-		<>
-		<h1>Calculator</h1>
-		<Button />
-		<Button />
-		</>
+		<div className="buttons-grid">
+			<Button text="ON"  type="function"/>
+			<Button text="MRC" type="function"/>
+			<Button text="M-"  type="function"/>
+			<Button text="M+"  type="function"/>
+
+			<Button text="OFF" type="function"/>
+			<Button text="√"   type="operation"/>
+			<Button text="%"   type="operation"/>
+			<Button text="÷"   type="operation"/>
+
+
+			<Button text="7"   type="number"/>
+			<Button text="8"   type="number"/>
+			<Button text="9"   type="number"/>
+			<Button text="×"   type="operation"/>
+
+			<Button text="4"   type="number"/>
+			<Button text="5"   type="number"/>
+			<Button text="6"   type="number"/>
+			<Button text="-"   type="operation"/>
+		
+			<Button text="1"   type="number"/>
+			<Button text="2"   type="number"/>
+			<Button text="3"   type="number"/>
+			<Button text="+"   type="operation" styleClasses="button-double-size"/> 
+
+			<Button text="0"   type="number"/>
+			<Button text="."   type="separator"/>
+			<Button text="="   type="operation"/>
+		</div>
 	);
 }
